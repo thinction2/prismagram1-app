61de19afef81d058f144e6e4e61cc652e3d46365
LeeJinSeok <dudwlsehdwk@nate.com>
2021-03-08 17:56:01 +0900

    Edit last

--- navigation/AuthNavigation.js
+++ navigation/AuthNavigation.js
@@ -1,9 +1,10 @@
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Signup from "../screens/Auth/Signup";
-import Confirm from "../screens/Auth/Signup";
-import Login from "../screens/Auth/Signup";
-import AuthHome from "../screens/Auth/Signup";
+import Confirm from "../screens/Auth/Confirm";
+import Login from "../screens/Auth/Login";
+import AuthHome from "../screens/Auth/AuthHome";
 
 const AuthNavigation = () => {
   const Stack = createStackNavigator();
@@ -11,7 +12,7 @@ const AuthNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="AuthHome" component={<AuthHome />} />
+        <Stack.Screen name="AuthHome" component={AuthHome} />
       </Stack.Navigator>
     </NavigationContainer>
   );
